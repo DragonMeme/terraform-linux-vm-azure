572b15310fe1cb55d10e930269cbffefa21a09cf
DragonMeme <48312022+DragonMeme@users.noreply.github.com>
2019-11-21 17:57:41 +1300

    Changed key value.

--- share-state.tf
+++ share-state.tf
@@ -1,9 +1,12 @@
 # Set the state to be stored in a blob hosted in Azure.
+# This assumes that there is a resource group name called RG_TEST that consists 
+# of a container named "tfstate" inside a storage account named "msa2019intertfstatetest".
+
 terraform {
   backend "azurerm" {
     storage_account_name = "msa2019interntfstatetest"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "test.terraform.tfstate"
     resource_group_name  = "RG_TEST"
   }
 }
