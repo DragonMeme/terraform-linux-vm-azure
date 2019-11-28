5864a992170ca06751bbda119b48bf49275790d3
DragonMeme <48312022+DragonMeme@users.noreply.github.com>
2019-11-29 10:40:13 +1300

    Update share-state.tf
    Changed comments to match backend

--- share-state.tf
+++ share-state.tf
@@ -1,6 +1,6 @@
 # Set the state to be stored in a blob hosted in Azure.
-# This assumes that there is a resource group name called RG_TEST that consists 
-# of a container named "tfstate" inside a storage account named "msa2019intertfstatetest".
+# This assumes that there is a resource group name called RG_LVM that consists 
+# of a container named "tfstate" inside a storage account named "statestoragelvm".
 
 terraform {
   backend "azurerm" {
